1691a6c124655f9871f65f2d2dc18ea82466a8e2
S20035\변승현 <s20035@gsm.hs.kr>
2021-01-11 15:25:21 +0900

    20200111/ Task까지 공부

--- app/src/main/java/com/example/a2021_01_app/Intent1.kt
+++ app/src/main/java/com/example/a2021_01_app/Intent1.kt
@@ -1,9 +1,12 @@
 package com.example.a2021_01_app
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_intent.*
+import kotlinx.android.synthetic.main.activity_intent2.*
 
 class Intent1 : AppCompatActivity() {
 
@@ -13,11 +16,36 @@ class Intent1 : AppCompatActivity() {
 
 
         change_activity.setOnClickListener{
-            val intent= Intent(this@Intent1, Intent2::class.java)
+
+//            val intent= Intent(this@Intent1, Intent2::class.java)
+//            //Key, Value -> Key와 Value를 쌍으로 만들어 저장한다. -> Dictionary
+//            intent.putExtra("number1",1)
+//            intent.putExtra("number2",2)
+//            startActivity(intent)
+
+//            val intent2 = Intent(this@Intent1, Intent2::class.java)
+//            // Apply ->
+//            intent2.apply{
+//                this.putExtra("number1", 1)
+//                this.putExtra("number2", 1)
+//            }
+//            startActivityForResult(intent2,200)
+
+            //암시적 인텐드
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"))
             startActivity(intent)
         }
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
 
+        if(requestCode == 200){
+            Log.d("number", ""+requestCode)
+            Log.d("number",""+resultCode)
+            val result = data?.getIntExtra("result",0)
+            Log.d("number",""+result)
+        }
 
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
--- app/src/main/java/com/example/a2021_01_app/Intent2.kt
+++ app/src/main/java/com/example/a2021_01_app/Intent2.kt
@@ -1,11 +1,37 @@
 package com.example.a2021_01_app
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_intent2.*
 
 class Intent2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent2)
+
+
+
+        result.setOnClickListener{
+            val number1 = intent.getIntExtra("number1",0)
+            val number2: Int = intent.getIntExtra("number2",0)
+
+            Log.d("number", ""+number1)
+            Log.d("number", ""+number2)
+
+            val result = number1 + number2
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("result", result)
+
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()// -> Activity 종료
+
+            //Stack
+            //Intent2
+            //Intent1
+        }
     }
 }
