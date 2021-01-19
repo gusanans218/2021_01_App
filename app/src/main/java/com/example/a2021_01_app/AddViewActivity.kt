45e817946272390f6bb8ae375c4f5214927cd30e
S20035\변승현 <s20035@gsm.hs.kr>
2021-01-19 23:45:58 +0900

    2020019/ Addview까지 공부

---
+++ app/src/main/java/com/example/a2021_01_app/AddViewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a2021_01_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_view)
+    }
+}
--- app/src/main/java/com/example/a2021_01_app/LibraryActivity.kt
+++ app/src/main/java/com/example/a2021_01_app/LibraryActivity.kt
@@ -1,18 +1,18 @@
-package com.example.a2021_01_app
-
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.bumptech.glide.Glide
-import com.bumptech.glide.Glide.with as with1
-
-class LibraryActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_library)
-
-        Glide.with(this@LibraryActivity)
-            .load("https://www.google.com/search?q=%EC%82%AC%EC%A7%84&tbm=isch&source=iu&ictx=1&fir=GZpW7ECvG3gVoM%252CrclhrRS630sGlM%252C_&vet=1&usg=AI4_-kQJrJk_Yanc-c4E6GXnSPDrdVlNYg&sa=X&ved=2ahUKEwjQq6KuraPuAhX1KKYKHScZAoYQ9QF6BAgIEAE#imgrc=GZpW7ECvG3gVoM")
-            .centerCrop()
-            .into(glide)
-    }
-}
+//package com.example.a2021_01_app
+//
+//import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+//import com.bumptech.glide.Glide
+//import com.bumptech.glide.Glide.with as with1
+//
+//class LibraryActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_library)
+//
+//        Glide.with(this@LibraryActivity)
+//            .load("https://www.google.com/search?q=%EC%82%AC%EC%A7%84&tbm=isch&source=iu&ictx=1&fir=GZpW7ECvG3gVoM%252CrclhrRS630sGlM%252C_&vet=1&usg=AI4_-kQJrJk_Yanc-c4E6GXnSPDrdVlNYg&sa=X&ved=2ahUKEwjQq6KuraPuAhX1KKYKHScZAoYQ9QF6BAgIEAE#imgrc=GZpW7ECvG3gVoM")
+//            .centerCrop()
+//            .into(glide)
+//    }
+//}
