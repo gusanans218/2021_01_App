8f16e8a92a2bfbdc150731232aff6fcaca42dc88
S20035\변승현 <s20035@gsm.hs.kr>
2021-01-24 17:43:28 +0900

    20210124/ RecyclerViewActivity까지 공부

---
+++ app/src/main/java/com/example/a2021_01_app/RecyclerViewActivity.kt
@@ -0,0 +1,56 @@
+package com.example.a2021_01_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_recycler_view.*
+
+class RecyclerViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view)
+
+        val carList = ArrayList<AddViewActivity.CarForList>()
+        for (i in 0 until 30){
+            carList.add(AddViewActivity.CarForList("" + i + "번째 자동차", "" + i + "순위 엔진"))
+        }
+        val adapter = RecyclerViewAdapter(carList, LayoutInflater.from(this@RecyclerViewActivity))
+        recycler_view.adapter = adapter
+        recycler_view.layoutManager = LinearLayoutManager(this@RecyclerViewActivity)
+    }
+}
+
+
+class RecyclerViewAdapter(
+    val itemList: ArrayList<AddViewActivity.CarForList>,
+    val inflater: LayoutInflater
+): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>(){
+        class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+            val carName: TextView
+            val carEngine: TextView
+
+            init{
+                carName = itemView.findViewById(R.id.car_name)
+                carEngine = itemView.findViewById(R.id.car_engine)
+            }
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = inflater.inflate(R.layout.item_view,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.carName.setText(itemList.get(position).name)
+        holder.carEngine.setText(itemList.get(position).engine)
+    }
+}
