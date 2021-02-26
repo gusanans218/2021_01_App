ae6593a11f44ffc4d87119d3aab4b8406e31f8cc
S20035\변승현 <s20035@gsm.hs.kr>
2021-02-27 01:34:23 +0900

    20210227/ Instargram 만들어보기

---
+++ app/src/main/java/com/example/a2021_01_app/EmailSignupActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a2021_01_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmailSignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_signup)
+    }
+}
